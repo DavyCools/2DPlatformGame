225ba7ae259e5f92865f936334f51392286f7845
Davy Cools <davy.cools@student.ap.be>
2018-12-27 19:08:29 +0100

    + Toevoegen commentaar SIngTileLevel2

--- 2DPlatformGame_Davy_Cools_2EA4/Blocks/NonStaticTiles/SignTileLevel2.cs
+++ 2DPlatformGame_Davy_Cools_2EA4/Blocks/NonStaticTiles/SignTileLevel2.cs
@@ -8,6 +8,11 @@ using Microsoft.Xna.Framework.Content;
 
 namespace _2DPlatformGame_Davy_Cools_2EA4
 {
+    /// <summary>
+    /// Deze klasse SignTileLevel2 is verantwoordelijk voor
+    /// het juiste gedrag van de SignTileLevel2
+    /// Erft over van: NonStaticTiles
+    /// </summary>
     class SignTileLevel2 : NonStaticTiles
     {
         public SignTileLevel2(ContentManager content, Vector2 _position, string name) : base(content, _position, name)
